0ca53504632e27719dd6bc6bf3d0ec312f21870c
LeksoBugadze <lesksobugadze@gmail.com>
2025-07-21 15:35:40 +0400

    updating PORT for deployment

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors=require('cors');
 const app=express();
 const User=require('./userSchema');
 
-const PORT=8000;
+const PORT=3000;
 
 const resend=new Resend(process.env.API_KEY);
 
@@ -44,7 +44,7 @@ async function startServer(){
         
         console.log('connected to mongoDB');
 
-        app.listen(PORT,()=>{
+        app.listen(PORT,'0.0.0.0',()=>{
             console.log(`Server is running on ${PORT}`);
         });
 
